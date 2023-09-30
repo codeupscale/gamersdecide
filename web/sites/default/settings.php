5005454a75ba0a93734351592d8ddb1bb7a1dca0
Umar Nawaz <umer.nawaz@codeupscale.com>
2023-09-30 12:24:45 +0500

    initial commit

---
+++ web/sites/default/settings.php
@@ -0,0 +1,18 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'gamersdecide',
+  'username' => 'root',
+  'password' => 'umar.N1280',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'isolation_level' => 'READ COMMITTED',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);
+$settings['hash_salt'] = 'gtoAMbNwMSl6O2cyHW-Ik-Zox0sf9CljARvsCs3Wq4ZPfY1XP_bqSjTJMeMLojEVs6iRxVU1TQ';
+$settings['config_sync_directory'] = 'sites/default/files/config_uv_tQ3kt_9erGBzIY2i97s1JiXriMuAx3VwtqBFj5bYLVV5k14h_NACtg1OmseYet7_afJHNIw/sync';
+$settings['trusted_host_patterns'] = array(
+  '.*',
+);
